8374b5e8757954a9f14f3f6ca08e1a3f3760c303
Nil <74917456+linvonavi@users.noreply.github.com>
2023-12-15 21:43:17 +0300

    Поменял порядок полей

--- app/src/main/java/com/scooter/datacollector/domain/models/GpsData.kt
+++ app/src/main/java/com/scooter/datacollector/domain/models/GpsData.kt
@@ -1,3 +1,3 @@
 package com.scooter.datacollector.domain.models
 
-data class GpsData(val speed: Double, val longitude: Double, val latitude: Double)
+data class GpsData(val speed: Double, val latitude: Double, val longitude: Double)
