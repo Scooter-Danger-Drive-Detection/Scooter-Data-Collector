5d1908c0ca135f2207b5989d042dd75fe401490c
aheront_aheront <k-maxim-k@mail.ru>
2023-12-15 22:20:01 +0300

    Добавлено считывание данных с GPS
    commit 1b5bdba59b17a2a1253f34b8c6e10997ac7f0d0a
    Author: aheront_aheront <k-maxim-k@mail.ru>
    Date:   Fri Dec 15 22:19:15 2023 +0300
    
        добавил обработку исключения
    
    commit 06e798dc8b8ca0d543818bdf80752852bd01540e
    Author: Nil <74917456+linvonavi@users.noreply.github.com>
    Date:   Fri Dec 15 21:44:45 2023 +0300
    
        Поменял порядок полей
    
    commit d18020646605f25866aa48f738aee2d0152320a0
    Author: Nil <74917456+linvonavi@users.noreply.github.com>
    Date:   Fri Dec 15 21:41:52 2023 +0300
    
        Первая версия
    
    commit be793da4cb654323e124bb291b88940e1a21387f
    Author: Nil <74917456+linvonavi@users.noreply.github.com>
    Date:   Fri Dec 15 21:39:49 2023 +0300
    
        Добавил разрешения для GPS

--- app/src/main/java/com/scooter/datacollector/sensors/gps/Gps.kt
+++ app/src/main/java/com/scooter/datacollector/sensors/gps/Gps.kt
@@ -1,10 +1,30 @@
 package com.scooter.datacollector.sensors.gps
 
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.widget.Toast
 import com.scooter.datacollector.domain.models.GpsData
 import com.scooter.datacollector.domain.sensors.IGps
 
-class Gps : IGps {
-    override fun getGpsData(): GpsData {
-        TODO("Not yet implemented")
+class Gps(private val context: Context) : IGps, LocationListener {
+    private val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    private var gpsData: GpsData = GpsData(0.0, 0.0, 0.0)
+
+    init {
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 0f, this)
+        } catch(ex: SecurityException) {
+            Toast.makeText(context, "Отсутсвует разрешение на использование GPS!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    override fun onLocationChanged(location: Location) {
+        gpsData = GpsData(location.speed.toDouble(), location.latitude, location.longitude)
+    }
+
+    override fun getGpsData() = gpsData
 }
