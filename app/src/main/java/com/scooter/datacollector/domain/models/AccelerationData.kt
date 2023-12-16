3301b590d0ecdb9348007341e064d5c668e89702
Nil <74917456+linvonavi@users.noreply.github.com>
2023-12-16 10:14:06 +0300

    Поменял AccelerationData

--- app/src/main/java/com/scooter/datacollector/domain/models/AccelerationData.kt
+++ app/src/main/java/com/scooter/datacollector/domain/models/AccelerationData.kt
@@ -1,10 +1,7 @@
 package com.scooter.datacollector.domain.models
 
 data class AccelerationData(
-    val fullAccelerationX : Double,
-    val fullAccelerationY: Double,
-    val fullAccelerationZ: Double,
-    val gravityX: Double,
-    val gravityY: Double,
-    val gravityZ: Double
+    val LinearAccelerationX: Double,
+    val LinearAccelerationY: Double,
+    val LinearAccelerationZ: Double
 )
