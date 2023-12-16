1c76b137e3dfd3a78d0a9534fb8e8667adad3bd9
Nil <74917456+linvonavi@users.noreply.github.com>
2023-12-16 10:14:54 +0300

    Accelerometer

--- app/src/main/java/com/scooter/datacollector/sensors/accelerometer/Accelerometer.kt
+++ app/src/main/java/com/scooter/datacollector/sensors/accelerometer/Accelerometer.kt
@@ -2,9 +2,29 @@ package com.scooter.datacollector.sensors.accelerometer
 
 import com.scooter.datacollector.domain.models.AccelerationData
 import com.scooter.datacollector.domain.sensors.IAccelerometer
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 
-class Accelerometer : IAccelerometer {
+class Accelerometer(private val sensorManager: SensorManager) : IAccelerometer, SensorEventListener {
+    private var accelerationData = AccelerationData(0.0, 0.0, 0.0)
+
+    init {
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_LINEAR_ACCELERATION) {
+            accelerationData = AccelerationData(event.values[0].toDouble(), event.values[1].toDouble(), event.values[2].toDouble())
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // handle accuracy changes
+    }
+
     override fun getAccelerationData(): AccelerationData {
-        TODO("Not yet implemented")
+        return accelerationData
     }
 }
